a794884e98a77dccb4e28c1abbb491b9dd4b9b5b
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 12:30:53 +0530

    task2 completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Task1 from './FunctionalComponent/Task1';
-import Task2 from './FunctionalComponent/Task2';
-import Task3 from './FunctionalComponent/Task3';
+import Task4 from './FunctionalComponent/Task4';
 function App() {
   return (
     <>
-      <p>Without Arrow Function</p>
-       <Task1></Task1>
-       <p>With Arrow Function</p>
-       <Task2></Task2>
-       <p>Using props</p>
-       <Task3 name="World"></Task3>  
+       <Task4></Task4>  
     </>
   );
 }
---
+++ src/FunctionalComponent/Task4.js
@@ -0,0 +1,25 @@
+import React from 'react'
+ function Task4() {
+  return (
+    <>
+    <div>
+       <h1>Mobile Operating System</h1>
+         <ul>
+         <li>Andriod</li>
+         <li>Blackberry</li>
+         <li>iPhone</li>
+         <li>Windows Phone</li>   
+         </ul> 
+        <h1>Mobile Manufacturer</h1>
+        <ul>
+         <li>samsung</li>
+         <li>HTC</li>
+         <li>Micromax</li>
+         <li>Apple</li>   
+        </ul> 
+
+    </div>
+    </>
+  )
+}
+export default Task4;
