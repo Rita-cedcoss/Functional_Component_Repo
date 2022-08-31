deac59fd94759fa07f7eb70c78f75c2a7124040b
Rita-cedcoss <ritapal@cedcoss.com>
2022-08-31 11:56:15 +0530

    Task1 part 2 completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Task1 from './FunctionalComponent/Task1';
+import Task2 from './FunctionalComponent/Task2';
 function App() {
   return (
-    <Task1></Task1>
+    <>
+      <p>Without Arrow Function</p>
+       <Task1></Task1>
+       <p>With Arrow Function</p>
+       <Task2></Task2>
+    </>
   );
 }
 export default App;
